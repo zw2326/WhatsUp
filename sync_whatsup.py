d38fae068dfe2471ae010fed3a2456323f542889
zw2326 <zw2288@columbia.edu>
2015-11-22 00:36:17 -0500

    Toggle light on/off menubar fixed at top

--- sync_whatsup.py
+++ sync_whatsup.py
@@ -6,5 +6,5 @@ import sys
 
 if __name__ == '__main__':
 	os.system('git add -A')
-	os.system('git commit -m "' + (sys.argv[1] if len(sys.argv) > 1 else '') + '"')
+	os.system('git commit -m "' + (' '.join(sys.argv[1:]) if len(sys.argv) > 1 else '') + '"')
 	os.system('git push')
